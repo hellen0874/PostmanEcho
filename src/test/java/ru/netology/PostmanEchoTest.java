d86b9d680988af600fc95f258896fc0b6e82e6a2
Elena Musinova <en.musinova@gmail.com>
2023-07-31 00:01:44 +0300

    Test is ready, build must fall

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,24 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    public void shouldReturnSentData() {
+
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("test") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("test1"));
+    }
+}
